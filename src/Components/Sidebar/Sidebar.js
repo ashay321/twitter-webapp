7a7a310edec71299c4ccfe2b2b39a6c556c2a954
ashay321 <ashay.wanjarkar1@getfareye.com>
2022-11-12 16:23:25 +0530

    added routes

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -17,12 +17,12 @@ function Sidebar() {
         <TwitterIcon className="twitter__icon" />
       </div>
       {/* List  */}
-      <SidebarOption active Icon={HomeIcon} text="Home" />
-      <SidebarOption Icon={ExploreIcon} text="Explore" />
-      <SidebarOption Icon={NotificationsIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
-      <SidebarOption Icon={PersonIcon} text="Profile" />
-      <SidebarOption Icon={SettingsIcon} text="Settings" />
+      <SidebarOption active Icon={HomeIcon} text="Home" navigationPath="/home"/>
+      <SidebarOption Icon={ExploreIcon} text="Explore" navigationPath="/explore"/>
+      <SidebarOption Icon={NotificationsIcon} text="Notifications" navigationPath="/notifications"/>
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" navigationPath="/messages"/>
+      <SidebarOption Icon={PersonIcon} text="Profile" navigationPath="/profile"/>
+      <SidebarOption Icon={SettingsIcon} text="Settings" navigationPath="/settings"/>
 
       {/* Tweet Button */}
 
