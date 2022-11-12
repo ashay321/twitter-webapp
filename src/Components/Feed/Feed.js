9c70013a3604b7f79d7c6c7ec5395fe9bec21890
Ganeshppk <115134515+Ganesh2884@users.noreply.github.com>
2022-11-12 11:18:07 +0530

    added  front page

---
+++ src/Components/Feed/Feed.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Post from "../Post/Post";
+import TweetBox from "../TweetBox/TweetBox";
+import "./Feed.css";
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+      <TweetBox />
+      <Post
+        displayName="Ganesh ppk"
+        username="Ganesh ppk"
+        verified={true}
+        text="Building a sasta twitter application"
+        avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1prwlaIHckvemT84pP_AP-WkMfFr1ao1q4mgqJEKx&s"
+      />
+      <Post
+        displayName="Ganesh ppk"
+        username="Ganesh ppk"
+        verified={true}
+        text="Building a sasta twitter application"
+        avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1prwlaIHckvemT84pP_AP-WkMfFr1ao1q4mgqJEKx&s"
+      />
+      <Post
+        displayName="Ganesh ppk"
+        username="Ganesh ppk"
+        verified={true}
+        text="Building a sasta twitter application"
+        avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1prwlaIHckvemT84pP_AP-WkMfFr1ao1q4mgqJEKx&s"
+      />
+      <Post
+        displayName="Ganesh ppk"
+        username="Ganesh ppk"
+        verified={true}
+        text="Building a sasta twitter application"
+        avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1prwlaIHckvemT84pP_AP-WkMfFr1ao1q4mgqJEKx&s"
+      />
+      <Post
+        displayName="Ganesh ppk"
+        username="Ganesh ppk"
+        verified={true}
+        text="Building a sasta twitter application"
+        avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1prwlaIHckvemT84pP_AP-WkMfFr1ao1q4mgqJEKx&s"
+      />
+      
+    </div>
+  );
+}
+
+export default Feed;
