431bf1cea9c26ddb5c49cedd8e0e04998363a869
Ganeshppk <115134515+Ganesh2884@users.noreply.github.com>
2022-11-12 22:00:10 +0530

    added login and signup page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Fragment } from "react";
-import Feed from "./Components/Feed/Feed";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import './App.css';
-import Widgets from "./Components/Widgets/Widgets";
+import "./App.css";
+// import Feed from "./Components/Feed/Feed";
+// import Sidebar from "./Components/Sidebar/Sidebar";
+// import Widgets from "./Components/Widgets/Widgets";
+// import Login from "./Components/Authnetication/LoginPage";
+import SignupPage from "./Components/Authnetication/SignupPage";
+
 function App() {
   return (
     <Fragment>
       <div className="App">
-        <Sidebar />
+        {/* <Sidebar />
         <Feed />
-        <Widgets />
+        <Widgets /> */}
+        {/* <Login /> */}
+        <SignupPage />
       </div>
     </Fragment>
   );
