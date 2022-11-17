e3dd4de4a4212c2a5899419f8086c00d1b2eb056
Ganeshppk <115134515+Ganesh2884@users.noreply.github.com>
2022-11-17 15:10:04 +0530

    Added styling of home page

--- src/Components/Widgets/WhoToFollowCard.js
+++ src/Components/Widgets/WhoToFollowCard.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import "./WhoToFollowCard.css";
+import { Button } from "@mui/material";
 export default function () {
   return (
     <div className="user__follow__card">
       <div className="bar__card">
         <Avatar className="avatar" />
-        <div>
+        <div className="header__text">
           <span className="username">Kushagra Singh</span>
-          <div>
-            <span>@Kushagra</span>
-          </div>
+          <span className="username__tag">@Kushagra</span>
         </div>
-        <div className="follow__btn">
+        {/* <div className="follow__btn">
           <h3>Follow</h3>
-        </div>
+        </div> */}
+        <Button variant="contained"  className="follow__btn">
+          follow
+        </Button>
       </div>
     </div>
   );
