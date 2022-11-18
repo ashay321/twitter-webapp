f1679efdfec333f255448b953a05f2aa64696bfd
ashay321 <ashay.wanjarkar1@getfareye.com>
2022-11-18 16:18:30 +0530

    updated comment box

--- src/Components/AddComment/AddCommentBox.js
+++ src/Components/AddComment/AddCommentBox.js
@@ -1,15 +1,12 @@
 import { Avatar, Button, Icon, IconButton } from "@mui/material";
 import React, { useState } from "react";
 import "./AddCommentBox.css";
-import PermMediaIcon from "@mui/icons-material/PermMedia";
 import axios from '../../axios'
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { storage } from '../../firebase';
-import { PhotoCamera } from "@mui/icons-material";
 import { useStateValue } from "../../StateProvider";
+import SendIcon from '@mui/icons-material/Send';
 
 
-function AddCommentBox({tweetId}) {
+function AddCommentBox({tweetId, setCommentsData}) {
     const [description, setDescription] = useState('')
     const [{userId}] = useStateValue();
 
@@ -23,24 +20,25 @@ function AddCommentBox({tweetId}) {
                 "tweetId":tweetId
             }
         })
-        console.log(response.status);
         if(response.status === 201) {
             setDescription("");
+            setCommentsData();
         } else {
             alert('Some error occured while posting the tweet')
         }
     }
 
     return (
-        <div className="tweetBox">
+        <div className="commentBox">
             <form>
-                <div className="tweetBox__input">
+                <div className="commentBox__input">
                     <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
                     <input className="tweet__input" placeholder="Reply.." type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                    {/* <Button className="commentBox__tweetButton" onClick={postNewTweet}>Comment</Button> */}
+                    <IconButton onClick={postNewTweet}><SendIcon sx={{ color: "black" }}/></IconButton>
                 </div>
 
                 
-                <Button className="tweetBox__tweetButton" onClick={postNewTweet}>Comment</Button>
             </form>
         </div>
     );
