e1223d73a74425919487b163c19f00512b3f08ab
ashay321 <ashay.wanjarkar1@getfareye.com>
2022-11-18 16:18:44 +0530

    changed baseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-//   baseURL: 'https://d28c-182-156-218-98.in.ngrok.io',
+  // baseURL: 'https://7acc-182-156-218-98.in.ngrok.io',
   baseURL: 'http://localhost:8080',
 });
 
